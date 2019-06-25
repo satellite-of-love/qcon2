3eeaf9e9fe5c11ff3a5f0d7d7787020fcb14e54a
Jessica Kerr <jessitron@gmail.com>
2019-06-25 18:42:02 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/yeah/yeah/Qcon2Application.java
@@ -0,0 +1,12 @@
+package yeah.yeah.;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qcon2Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Qcon2Application.class, args);
+	}
+
+}
